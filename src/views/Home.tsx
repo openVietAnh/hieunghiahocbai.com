7f1d450b23d6f3bfec3699ec90958ac6cd71e5f5
openVietAnh <vietanhtran.uet@gmail.com>
2025-05-05 11:53:36 +0700

    feat: Implement the project bare bone structure/routing + Home page

---
+++ src/views/Home.tsx
@@ -0,0 +1,75 @@
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material";
+
+export const Home: React.FC = () => {
+  return (
+    <Grid
+      flexDirection="column"
+      height="100vh"
+      container
+      spacing={4}
+      alignContent="center"
+      alignSelf="center"
+      textAlign="center"
+      justifyContent="center"
+    >
+      <h1>Ai sẽ làm bài?</h1>
+      <Grid
+        container
+        spacing={4}
+        alignContent={"center"}
+        alignSelf={"center"}
+        justifyContent="center"
+      >
+        <Card
+          style={{
+            width: "300px",
+          }}
+        >
+          <a href="/practice?assignee=Hieu">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="300"
+                image="/public/hieu.jpg"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Trần Phúc Hiếu
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </a>
+        </Card>
+        <Card
+          style={{
+            width: "300px",
+          }}
+        >
+          <a href="/practice?assignee=Hieu">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="300"
+                image="/public/nghia.jpg"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Trần Nhân Nghĩa
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </a>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
---
+++ src/views/Practice.tsx
@@ -0,0 +1,37 @@
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export const Practice: React.FC = () => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, width: "100%" }}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Luyện Tập
+            </Typography>
+            <a href="/">
+              <Button color="inherit">Thoát</Button>
+            </a>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
